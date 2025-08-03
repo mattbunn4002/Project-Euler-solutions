b3c08a673e6b62f71306d57ab4dbeadd919b5426
mattbunn4002 <mattbunn4002@gmail.com>
2025-08-03 22:14:44 +0100

    Adding solutions 1 & 2

---
+++ Three-or-five.py
@@ -0,0 +1,11 @@
+#Find the sum of all the multiples of 3 or 5 below 1000.
+
+sum = 0
+for num in range(1000):
+    if (num % 3 == 0 or num % 5 == 0):
+        print(num)
+        sum += num
+        continue
+
+
+print(sum)
